73c10bc7a67b66a4dd8ba1be55ce08023d0ff99b
Rifdatun Nafi'ah <149130538+rifdatnnfh@users.noreply.github.com>
2024-06-03 01:30:20 +0800

    first commit

---
+++ uas pti cloudy cupcake/index.js
@@ -0,0 +1,103 @@
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-bar").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+let search = document.querySelector(".search");
+document.querySelector("#search").onclick = () => {
+  search.classList.toggle("active");
+};
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slidesShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".product-row", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  loop: true,
+  centeredSlides: true,
+
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".blogs-row", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+var swiper = new Swiper(".review-row", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
